75a48ca619890aa55dec5b4b8be420b10e53f977
Steven Chu <stevenc1@gmail.com>
2022-01-18 21:57:13 -0800

    working code using buffer approach (#3)
    * working code using buffer approach
    
    better comments
    
    * it should now be working
    
    * cleaning up error messaging and exception handling
    
    * add some logging
    
    * add docstrings

--- s3_archiver/archiver.py
+++ s3_archiver/archiver.py
@@ -2,8 +2,29 @@ import typing as t
 
 import argparse
 import json
+import logging
+import sys
+import time
+import threading
+
+from datetime import datetime
 
 from kafka import KafkaConsumer
+
+from utils.s3 import create_bucket_if_not_exists
+from utils.s3 import upload_messages_to_s3
+
+
+# -----------------------------------------------------------------------------
+#   Constants
+# -----------------------------------------------------------------------------
+
+
+logging.basicConfig(level=logging.INFO)
+
+BUFFER_LOCK: threading.Lock = threading.Lock()
+BYTE_SIZE_TO_FLUSH: float = 2.5e7  # 25MB
+SLEEP_TIME_IN_SECONDS: int = 60
 
 
 # -----------------------------------------------------------------------------
@@ -22,6 +43,13 @@ def get_cli_args() -> t.Any:
     )
 
     parser.add_argument(
+        "--s3-bucket",
+        dest="s3_bucket",
+        default=None,
+        help="The S3 bucket into which we will write our data",
+    )
+
+    parser.add_argument(
         "--bootstrap-server",
         dest="bootstrap_server",
         default="localhost:9092",
@@ -32,33 +60,72 @@ def get_cli_args() -> t.Any:
 
 
 # -----------------------------------------------------------------------------
-#   Begin daemon
+#   Uploading daemon
+# -----------------------------------------------------------------------------
+
+
+def listen_and_maybe_upload(buffer: t.List[str], bucket_name: str) -> None:
+    """Polls in 1m intervals and checks whether (a) it's been 15 minutes since
+    the last flush to S3 or (b) if the buffer has gotten too big [25MB]. If
+    either condition is met, then flushes to S3."""
+    while True:
+        now = datetime.now()
+        current_minute = now.minute
+        with BUFFER_LOCK:
+            if current_minute % 15 == 0 or sys.getsizeof(buffer) >= BYTE_SIZE_TO_FLUSH:
+                upload_messages_to_s3(buffer, bucket_name)
+                buffer.clear()
+        time.sleep(SLEEP_TIME_IN_SECONDS)
+
+
+def start_uploader_daemon(buffer: t.List[str], bucket_name: str) -> threading.Thread:
+    logging.info("Starting uploader daemon")
+    t = threading.Thread(
+        target=listen_and_maybe_upload,
+        args=(
+            buffer,
+            bucket_name,
+        ),
+    )
+    t.start()
+    return t  # though we don't actually do anything with it atm
+
+
+# -----------------------------------------------------------------------------
+#   Consuming daemon
+# -----------------------------------------------------------------------------
+
+
+def consume_messages(
+    kafka_topic: str, kafka_bootstrap_server: str, buffer: t.List[str]
+) -> None:
+    consumer = KafkaConsumer(
+        # Other params of interest:
+        # - auto_offset_reset
+        # - enable_auto_commit
+        # - consumer_timeout_ms
+        kafka_topic,
+        bootstrap_servers=[kafka_bootstrap_server],
+        value_deserializer=lambda x: json.loads(x.decode("utf-8")),
+    )
+    logging.info("Starting to consume from Kafkfa")
+    for message in consumer:
+        with BUFFER_LOCK:
+            buffer.append((message.offset, message.value["data"]))
+
+
+# -----------------------------------------------------------------------------
+#   Entrypoint
 # -----------------------------------------------------------------------------
 
 
 def main() -> None:
     args = get_cli_args()
-
-    # Other params of interest:
-    # - auto_offset_reset
-    # - enable_auto_commit
-    # - consumer_timeout_ms
-    consumer = KafkaConsumer(
-        args.topic,
-        bootstrap_servers=[args.bootstrap_server],
-        value_deserializer=lambda x: json.loads(x.decode("utf-8")),
-    )
-    for message in consumer:
-        print(
-            "%s:%d:%d: key=%s value=%s"
-            % (
-                message.topic,
-                message.partition,
-                message.offset,
-                message.key,
-                message.value,
-            )
-        )
+    message_buffer = []
+    s3_bucket_name = args.s3_bucket
+    create_bucket_if_not_exists(s3_bucket_name)
+    start_uploader_daemon(message_buffer, s3_bucket_name)
+    consume_messages(args.topic, args.bootstrap_server, message_buffer)
 
 
 if __name__ == "__main__":
---
+++ s3_archiver/utils/s3.py
@@ -0,0 +1,67 @@
+import typing as t
+
+import json
+import logging
+import os
+
+from datetime import datetime
+
+import boto3
+
+
+# -----------------------------------------------------------------------------
+#   Constants
+# -----------------------------------------------------------------------------
+
+
+logging.basicConfig(level=logging.INFO)
+
+DEFAULT_REGION = os.environ.get("DEFAULT_AWS_REGION", "us-west-2")
+S3_CLIENT = boto3.client("s3", region_name=DEFAULT_REGION)
+
+
+# -----------------------------------------------------------------------------
+#   Utils
+# -----------------------------------------------------------------------------
+
+
+def create_bucket_if_not_exists(bucket_name: str) -> None:
+    logging.info(
+        f"Called into `create_bucket_if_not_exists` method with arg: [{bucket_name}]"
+    )
+    try:
+        response = S3_CLIENT.create_bucket(
+            Bucket=bucket_name,
+            CreateBucketConfiguration={
+                "LocationConstraint": DEFAULT_REGION,
+            },
+        )
+        if not response.get("ResponseMetadata", {}).get("HTTPStatusCode") == 200:
+            raise Exception(f"Response was not OK: {response}")
+    except (
+        S3_CLIENT.exceptions.BucketAlreadyExists,
+        S3_CLIENT.exceptions.BucketAlreadyOwnedByYou,
+    ):
+        logging.info(f"Bucket {bucket_name} already exists")
+
+
+def upload_messages_to_s3(
+    messages: t.List[t.Tuple[int, str]], bucket_name: str
+) -> None:
+    """Input is expected to be a list of tuples where the first element of the tuple
+    will be used for namespacing the S3 objects. We write this as a newline-separated
+    series of JSON strings."""
+    if not messages:
+        logging.info(
+            "Returning early from `upload_messages_to_s3` -- no messages to upload"
+        )
+    last_message_offset: int = messages[-1][0]
+    today_as_string = datetime.now().date().isoformat()
+    logging.info(f"About to start writing {len(messages)} messages into S3")
+    response = S3_CLIENT.put_object(
+        Body="\n".join(json.dumps(x[1]) for x in messages),
+        Bucket=bucket_name,
+        Key=f"dt={today_as_string}/{last_message_offset}.json",
+    )
+    if not response.get("ResponseMetadata", {}).get("HTTPStatusCode") == 200:
+        logging.error(f"Response was not OK: {response}")
